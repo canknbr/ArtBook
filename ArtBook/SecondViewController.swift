7c0bcb556b7a7b4349e5c552480b7dcf16755e58
canknbr <cankanbur1@gmail.com>
2023-03-30 23:42:23 +0300

    added segue prepare and style storyboards

---
+++ ArtBook/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  ArtBook
+//
+//  Created by Can Kanbur on 30.03.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet var yearTextField: UITextField!
+    @IBOutlet var artistTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveButton(_ sender: Any) {
+    }
+}
